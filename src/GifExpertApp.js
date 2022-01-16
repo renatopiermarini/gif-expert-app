808768ed2bf7ce0043d7904aa33af4707ca380a4
Renato Piermarini <renatopiermarinih@gmail.com>
2022-01-16 19:05:22 -0300

    first-commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import AddCategory from "./Components/AddCategory";
+import GifGrid from "./Components/GifGrid";
+
+const GifExpertApp = () => {
+  const [categories, setCategories] = useState(["Ferrari"]);
+
+  return (
+    <>
+      <h2>GifExpertApp</h2>
+      <AddCategory setCategories={setCategories} />
+      <hr />
+
+      <ol>
+        {categories.map((category) => (
+          <GifGrid key={category} category={category} />
+        ))}
+        ;
+      </ol>
+    </>
+  );
+};
+export default GifExpertApp;
---
+++ src/helpers/getGifs.js
@@ -0,0 +1,19 @@
+export const getGifs = async (category) => {
+  const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(
+    category
+  )}&limit=10&api_key=IkZ34A5N1lF7dFIoQQIeLKJK13yH8qmj`;
+
+  const respo = await fetch(url);
+
+  const { data } = await respo.json();
+
+  const gifs = data.map((img) => {
+    return {
+      id: img.id,
+      title: img.title,
+      url: img.images?.downsized_medium.url,
+    };
+  });
+
+  return gifs;
+};
